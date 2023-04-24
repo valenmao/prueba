5da6bb0166dc00a11290a469ac5487064f856f3c
Martina-mey <matumeyer0109@gmail.com>
2023-04-24 19:28:50 -0300

    primeros planteos de cliente.cpp y empleado.cpp

---
+++ Empleado.cpp
@@ -0,0 +1,30 @@
+#include "Empleado.h"
+
+//constructor
+Empleado::Empleado
+
+//destructor
+Empleado::~Empleado() {
+
+}
+
+//getters
+date Empleado::get_HorarioLaboral() {
+	return this->HorarioLaboral;
+}
+
+float Empleado::get_Sueldo() {
+	return this->Sueldo;
+}
+
+//setters
+
+	//crear un parametro de NuevaHoraLaboral
+date Empleado::set_HorarioLaboral(date NuevaHoraLaboral) {
+	this->HorarioLaboral = NuevoHorarioLaboral;
+}
+
+	//crear un parametro de NuevoSueldo
+float Empleado::set_Sueldo(float NuevoSueldo) {
+	this->Sueldo = NuevoSueldo;
+}
